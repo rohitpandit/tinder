e99cbce3543d4be512bf2eb552bf005a2a232bf5
Rohit Pandit <rohit0311@outlook.com>
2021-01-28 14:27:43 +0530

    servers/profile-service express setup

---
+++ servers/profile-service/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+	res.status(200);
+	res.json({
+		msg: 'hello',
+	});
+});
+
+app.listen(port, console.log(`profile server listening at port: ${port}`));
