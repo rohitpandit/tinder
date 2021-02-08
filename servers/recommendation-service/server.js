22a01fbea6bc51de51c9593ac95476d259bf7c7f
Rohit Pandit <rohit0311@outlook.com>
2021-02-08 14:16:07 +0530

    basic recommendation server

---
+++ servers/recommendation-service/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+
+const port = 5002;
+
+app.use(morgan('dev'));
+app.use(express.json({ limit: '50mb' }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+app.get('/', (req, res) => {
+	res.status(200).json({ msg: 'recommendation' });
+});
+
+app.listen(port, () => {
+	console.log(`Recommendation service listening at port: ${port}`);
+});
