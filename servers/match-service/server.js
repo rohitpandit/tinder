f877079182078c811a7d2392dec43ff9299b4fc6
Rohit Pandit <rohit0311@outlook.com>
2021-02-15 09:24:03 +0530

    silly mistake solved

--- servers/match-service/server.js
+++ servers/match-service/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 const matchRouter = require('./routes/match');
+
 const db = require('./db');
 
 const port = 5003;
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.status(200).json({ msg: 'hi' });
