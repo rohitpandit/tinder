152a895a9ecd85ae1f97cfbe82993dbe242dbcae
Rohit Pandit <rohit0311@outlook.com>
2021-02-16 13:58:16 +0530

    match page

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -7,10 +7,73 @@ const Match = ({ setIsLogged, setTotalViewed }) => {
 	return (
 		<div>
 			<Navbar setTotalViewed={setTotalViewed} setIsLogged={setIsLogged} />
-			Mathces
+
+			<div className='container mb-5 mt-2' style={style}>
+				<h1>My Mathes</h1>
+				<hr />
+				<div
+					className='container d-flex border rounded m-3 bg-light'
+					style={elementStyle}>
+					<div className='border rounded-circle overflow-hidden m-1'>
+						<i className='card-img-top fas fa-user fa-3x d-flex justify-content-center align-items-center bg-dark text-light'></i>
+					</div>
+					<div className='d-flex justify-content-center align-items-center ml-2'>
+						name 1
+					</div>
+				</div>
+				<div
+					className='container d-flex border rounded m-3  bg-light'
+					style={elementStyle}>
+					<div className='border rounded-circle overflow-hidden m-1'>
+						<i className='card-img-top fas fa-user fa-3x d-flex justify-content-center align-items-center bg-dark text-light'></i>
+					</div>
+					<div className='d-flex justify-content-center align-items-center ml-2'>
+						name 1
+					</div>
+				</div>
+				<div
+					className='container d-flex border rounded m-3  bg-light'
+					style={elementStyle}>
+					<div className='border rounded-circle overflow-hidden m-1'>
+						<i className='card-img-top fas fa-user fa-3x d-flex justify-content-center align-items-center bg-dark text-light'></i>
+					</div>
+					<div className='d-flex justify-content-center align-items-center ml-2'>
+						name 1
+					</div>
+				</div>
+				<div
+					className='container d-flex border rounded m-3 bg-light'
+					style={elementStyle}>
+					<div className='border rounded-circle overflow-hidden m-1'>
+						<i className='card-img-top fas fa-user fa-3x d-flex justify-content-center align-items-center bg-dark text-light'></i>
+					</div>
+					<div className='d-flex justify-content-center align-items-center ml-2'>
+						name 1
+					</div>
+				</div>
+				<div
+					className='container d-flex border rounded m-3 bg-light'
+					style={elementStyle}>
+					<div className='border rounded-circle overflow-hidden m-1'>
+						<i className='card-img-top fas fa-user fa-3x d-flex justify-content-center align-items-center bg-dark text-light'></i>
+					</div>
+					<div className='d-flex justify-content-center align-items-center ml-2'>
+						name 1
+					</div>
+				</div>
+			</div>
 			<Footer />
 		</div>
 	);
+};
+
+const style = {
+	// height: '90vh',
+};
+
+const elementStyle = {
+	padding: '.5rem',
+	margin: '.5rem',
 };
 
 export default Match;
