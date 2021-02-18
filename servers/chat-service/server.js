13d4a0635c51fa43d983ec8dac11b19097829525
Rohit Pandit <rohit0311@outlook.com>
2021-02-18 14:23:38 +0530

    basic socket connection

---
+++ servers/chat-service/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+	cors: {
+		origin: 'http://localhost:3000',
+	},
+});
+
+const port = 5004;
+
+http.listen(port, () => {
+	console.log(`chat server listening at port: ${port}`);
+});
